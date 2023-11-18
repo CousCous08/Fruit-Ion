ac24df6f428d3af3f70d98f59df204f1c03ad549
Choi, Sugju <schoi424@gatech.edu>
2023-11-18 14:47:22 -0500

    Edit UI to fit web screen and change menu design

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import ChatInterface from '../components/ChatInterface';
-import Menu from'../components/Menu';
+import './Home.css';
 
 // import './home.css';
 class Home extends Component {
     render() {
-        return(
-            <div className="home-page">
-                <ChatInterface />
-            </div>
+        return (
+            <ChatInterface class="chat-in-home" />
         );
     }
 }
