018ec7df907e7bb6ec82cb1cbe26ec1e8ab970ca
adgnvtefc <zhang.hanmo2018@gmail.com>
2023-11-18 14:02:38 -0500

    begin integration of frontend & backend

--- app/src/app.py
+++ app/src/app.py
@@ -1,13 +1,29 @@
 #skeleton flask backend
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+
 app = Flask(__name__)
 CORS(app)
 
+anthropic = Anthropic(
+    # defaults to os.environ.get("ANTHROPIC_API_KEY")
+    api_key="REDACTED",
+)
+client = Anthropic()
+
 #example function
-@app.route('/api/data')
+@app.route('/api/test_prompt', methods=['POST'])
 def data():
-    return jsonify({'message': 'Hello from Flask!'})
+    data = request.get_json()
+    input = data['message']
+    test_prompt = f"""{HUMAN_PROMPT} translate the statement in XML tags into Chinese: <statement> {input} <\statement> {AI_PROMPT} """
+    json_completion = anthropic.completions.create(
+        model="claude-2",
+        max_tokens_to_sample=300,
+        prompt = test_prompt,
+    )
+    return jsonify(json_completion.completion)
 
 if __name__ == '__main__':
     app.run()
