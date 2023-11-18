f97117dbf0aba98d55d71b1dab04f5535f8199d8
jihyeo2 <julie2017m@gmail.com>
2023-11-18 11:21:40 -0500

    edited router and organized files

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from './pages/Home';
+import About from './pages/About';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes> 
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes> 
+  </BrowserRouter>
+)
+
+export default App;
