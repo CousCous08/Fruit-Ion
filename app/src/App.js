b4db16ed7f9c3c5d7ae106b4f0d54c76df305337
Choi, Sugju <schoi424@gatech.edu>
2023-11-18 14:47:22 -0500

    Edit UI to fit web screen and change menu design

--- app/src/App.js
+++ app/src/App.js
@@ -3,14 +3,20 @@ import Home from './pages/Home';
 import About from './pages/About';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Menu from './components/Menu';
+import './App.css';
 
 const App = () => (
   <BrowserRouter>
-    <Menu />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <div className="app-container">
+      <Menu/>
+      <div className="main-content">
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        
+      </Routes>
+      </div>
+    </div>
   </BrowserRouter>
 )
 
