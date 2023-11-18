59b0622a5abcd5503954eeafde872371f616ea53
Choi, Sugju <schoi424@gatech.edu>
2023-11-18 18:07:10 -0500

    Add Milestones page
    when user clicks goal card, moves to milestone page

--- app/src/pages/GoalsPage.js
+++ app/src/pages/GoalsPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import GoalCard from '../components/GoalCard';
 import './GoalsPage.css';
 
@@ -7,41 +8,53 @@ import './GoalsPage.css';
 const GoalsPage = () => {
     const [headerWidth, setHeaderWidth] = useState(0);
     const containerRef = useRef(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const updateWidth = () => {
-          if (containerRef.current) {
-            setHeaderWidth(containerRef.current.offsetWidth);
-          }
+            if (containerRef.current) {
+                setHeaderWidth(containerRef.current.offsetWidth);
+            }
         };
-    
+
         window.addEventListener('resize', updateWidth);
         updateWidth();
-    
+
         return () => window.removeEventListener('resize', updateWidth);
-      }, []);
+    }, []);
 
     const goals = [
-        { id: 1, name: "Finish Project", deadline: "2023-01-15" },
-        { id: 2, name: "Start New Course", deadline: "2023-02-10" },
-        { id: 3, name: "Start New Course2", deadline: "2023-02-11" },
-        { id: 4, name: "Start New Course3", deadline: "2023-02-12" },
-        { id: 5, name: "Finish Project", deadline: "2023-01-15" },
-        { id: 6, name: "Start New Course", deadline: "2023-02-10" },
-        { id: 7, name: "Start New Course2", deadline: "2023-02-11" },
-        { id: 8, name: "Start New Course3", deadline: "2023-02-12" },
+        { id: 1, name: "Finish Project", deadline: "2023-01-15", milestones: ["Research topic", "Create outline", "Write first draft"] },
+        { id: 2, name: "Start New Course", deadline: "2023-02-10", milestones: ["Research topic", "Create outline", "Write first draft"] },
+        { id: 3, name: "Start New Course2", deadline: "2023-02-11", milestones: ["Research topic", "Create outline", "Write first draft"] },
+        { id: 4, name: "Start New Course3", deadline: "2023-02-12", milestones: ["Research topic", "Create outline", "Write first draft"] },
+        { id: 5, name: "Finish Project", deadline: "2023-01-15", milestones: ["Research topic", "Create outline", "Write first draft"] },
+        { id: 6, name: "Start New Course", deadline: "2023-02-10", milestones: ["Research topic", "Create outline", "Write first draft"] },
+        { id: 7, name: "Start New Course2", deadline: "2023-02-11", milestones: ["Research topic", "Create outline", "Write first draft"] },
+        { id: 8, name: "Start New Course3", deadline: "2023-02-12", milestones: ["Research topic", "Create outline", "Write first draft"] },
     ];
+
+    const handleGoalClick = (goal) => {
+        // Navigate to the detailed goal page
+        navigate(`/milestones/${goal.id}`, { state: { goal } });
+    };
 
     return (
         <div ref={containerRef} className="goals-container">
-          <h1 className="goals-header" style={{ width: `${headerWidth}px` }}>Goals</h1>
-          <div className="goals-list">
-            {goals.map(goal => (
-              <GoalCard key={goal.name} name={goal.name} deadline={goal.deadline} />
-            ))}
-          </div>
+            <h1 className="goals-header" style={{ width: `${headerWidth}px` }}>Goals</h1>
+            <div className="goals-list">
+                {goals.map(goal => (
+                    <GoalCard
+                        key={goal.id}
+                        name={goal.name}
+                        deadline={goal.deadline}
+                        milestones={goal.milestones}
+                        onClick={() => handleGoalClick(goal)}
+                    />
+                ))}
+            </div>
         </div>
-      );
+    );
 }
 
 export default GoalsPage;
---
+++ app/src/pages/MilestonesPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const MilestonesPage = () => {
+  const location = useLocation();
+  const goal = location.state.goal;
+
+  return (
+    <div>
+      <h1>{goal.name}</h1>
+      <p>Deadline: {goal.deadline}</p>
+      <h3>Milestones</h3>
+      <ul>
+        {goal.milestones.map((milestone, index) => (
+          <li key={index}>{milestone}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MilestonesPage;
