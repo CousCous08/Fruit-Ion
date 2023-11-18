21b3561dd73152212e2c67ce4a749d9c96cff565
Choi, Sugju <schoi424@gatech.edu>
2023-11-18 12:07:42 -0500

    Add Menu

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ChatInterface from '../components/ChatInterface';
+import Menu from'../components/Menu';
 
 // import './home.css';
 class Home extends Component {
     render() {
         return(
             <div className="home-page">
+                <Menu/>
                 <ChatInterface />
             </div>
         );
