ceb92d1a7ae02476246a8dcfdbb5034f8808e116
Choi, Sugju <schoi424@gatech.edu>
2023-11-19 05:13:58 -0500

    Add habit-tag

--- app/src/components/MilestoneCard.js
+++ app/src/components/MilestoneCard.js
@@ -1,22 +1,22 @@
 import React from 'react';
-
+import './MilestoneCard.css'; // Import the CSS file
 
 const MilestoneCard = ({ name, deadline, onClick }) => {
-  return (
-    <div onClick={onClick} style={{ cursor: 'pointer' }}>
-      <div class="flex flex-col rounded-xl bg-white p-4 shadow-lg w-full">
-        <div class="mb-2 flex justify-between">           
-          <h3 class="text-sm">Until {deadline}</h3>
-          <div className='px-2 py-1 border-2 border-yellow-300 rounded-md'>
-            <h3 className='font-light text-yellow-300 text-sm'>MileStone</h3>
-          </div>                
+    return (
+        <div onClick={onClick} className="milestone-card">
+            <div className="card-body">
+                <div className="card-header">           
+                    <h3 className="deadline">Until {deadline}</h3>
+                    <div className="milestone-tag">
+                        <h3 className="tag-text">MileStone</h3>
+                    </div>                
+                </div>
+                <div>
+                    <h2 className="milestone-name">{name}</h2>
+                </div>
+            </div>
         </div>
-        <div>
-          <h2 className="font-semibold">{name}</h2>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default MilestoneCard;
